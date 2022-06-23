ad9cca65b58433ac7742e02a73e8c804fac9a210
Sabahattin Çiçek <scicek12345@gmail.com>
2022-06-23 15:49:24 +0300

    created singleton for datas

--- Lavira Merkut/Form1.cs
+++ Lavira Merkut/Form1.cs
@@ -17,6 +17,7 @@ using CefSharp;
 using CefSharp.SchemeHandler;
 using CefSharp.Web;
 using CefSharp.WinForms;
+using Lavira_Merkut.Singleton;
 using MapWinGIS;
 using Image = MapWinGIS.Image;
 using Point = MapWinGIS.Point;
@@ -33,6 +34,7 @@ namespace Lavira_Merkut
         int minute = 0;
         int hour = 0;
 
+        IncomingDataSingleton incomingData;
 
         public string COMPort="COM6";
 
@@ -121,6 +123,7 @@ namespace Lavira_Merkut
                 MessageBox.Show(e.Message);
                 return;
             }
+            incomingData = IncomingDataSingleton.GetInstance();
             timer.Start();
             state = true;
             streamLoop();
@@ -213,54 +216,81 @@ namespace Lavira_Merkut
             string time = /*hour + ":" +*/ minute.ToString("00") + ":" + second.ToString("00") + "." + milisecond.ToString("00");
             return time;
         }
-      
-        //00 altitude
-        //01 gps altitude
-        //02 gps latitude
-        //03 gps longitude
-        //04 payload gps altitude
-        //05 payload gps latitude
-        //06 payload pgs longitude
-        //07 air pressure
-        //08 gyroscope x
-        //09 gyroscope y
-        //10 gyroscope z
-        //11 acceleration x
-        //12 acceleration y
-        //13 acceleration z
-        //14 angle
-        //15 state
-        //16 velocity
+
+        
+
+        //00 teamID
+        //01 counter
+        //02 altitude
+        //03 gps altitude
+        //04 gps latitude
+        //05 gps longitude
+        //06 payload gps altitude
+        //07 payload gps latitude
+        //08 payload pgs longitude
+        //09 gyroscope x
+        //10 gyroscope y
+        //11 gyroscope z
+        //12 acceleration x
+        //13 acceleration y
+        //14 acceleration z
+        //15 air pressure
+        //16 angle
+        //17 velocity
+        //18 state
+        //19 crc
+
         async void streamLoop()
         {
         start:
             if (state)
             {
                try{
-                    Random random = new Random();
                     read: strReceived = stream.ReadLine();
                     await Task.Delay(200);
                     strData = strReceived.Split('_');
-                    while (strData.Length != 17) {goto read;  } /*MessageBox.Show(strReceived);*/
-                    textBox_altitude.Text = strData[0];
-                    textBox_gps_altitude.Text = strData[1];
-                    textBox_gps_latitude.Text = strData[2];
-                    textBox_gps_longitude.Text = strData[3];
-                    textBox_payload_gps_altidue.Text = strData[4];
-                    textBox_payload_gps_latitude.Text = strData[5];
-                    textBox_payload_gps_longitude.Text = strData[6];
-                    textBox_air_pressure.Text = strData[7];
-                    textBox_gyroscope_X.Text = strData[8];
-                    textBox_gyroscope_Y.Text = strData[9];
-                    textBox_gyroscope_Z.Text = strData[10];
-                    textBox_acceleration_X.Text = strData[11];
-                    textBox_acceleration_Y.Text = strData[12];
-                    textBox_acceleration_Z.Text = strData[13];
-                    textBox_angle.Text = strData[14];
-                    chart_velocity.Series["Velocity"].Points.AddXY(currentTime, strData[16]);
-                    chart_altitude.Series["Altitude"].Points.AddXY(currentTime, strData[0]);
+                    while (strData.Length != 20) {goto read;}
 
-                    browser.EvaluateScriptAsync("setmark("+strData[2]+","+ strData[3] + ");");
+                    incomingData.TeamID = strData[0];
+                    incomingData.Counter = strData[1];
+                    incomingData.Altitude = strData[2];
+                    incomingData.Gps_altitude = strData[3];
+                    incomingData.Gps_latitude = strData[4];
+                    incomingData.Gps_longitude = strData[5];
+                    incomingData.Payload_gps_altitude = strData[6];
+                    incomingData.Payload_gps_latitude = strData[7];
+                    incomingData.Payload_gps_longitude = strData[8];
+                    incomingData.Gyroscope_X = strData[9];
+                    incomingData.Gyroscope_Y = strData[10];
+                    incomingData.Gyroscope_Z = strData[11];
+                    incomingData.Acceleration_X = strData[12];
+                    incomingData.Acceleration_Y = strData[13];
+                    incomingData.Acceleration_Z = strData[14];
+                    incomingData.AirPressure = strData[15];
+                    incomingData.Angle = strData[16];
+                    incomingData.Velocity = strData[17];
+                    incomingData.State = strData[18];
+                    incomingData.Crc = strData[19];
+
+                    textBox_altitude.Text = incomingData.Altitude;
+                    textBox_gps_altitude.Text = incomingData.Gps_altitude;
+                    textBox_gps_latitude.Text = incomingData.Gps_latitude;
+                    textBox_gps_longitude.Text = incomingData.Gps_longitude;
+                    textBox_payload_gps_altidue.Text = incomingData.Payload_gps_altitude;
+                    textBox_payload_gps_latitude.Text = incomingData.Payload_gps_latitude;
+                    textBox_payload_gps_longitude.Text = incomingData.Payload_gps_longitude;
+                    textBox_air_pressure.Text = incomingData.AirPressure;
+                    textBox_gyroscope_X.Text = incomingData.Gyroscope_X;
+                    textBox_gyroscope_Y.Text = incomingData.Gyroscope_Y;
+                    textBox_gyroscope_Z.Text = incomingData.Gyroscope_Z;
+                    textBox_acceleration_X.Text = incomingData.Acceleration_X;
+                    textBox_acceleration_Y.Text = incomingData.Acceleration_Y;
+                    textBox_acceleration_Z.Text = incomingData.Acceleration_Z;
+                    textBox_angle.Text = incomingData.Angle;
+                    chart_velocity.Series["Velocity"].Points.AddXY(currentTime, incomingData.Velocity);
+                    chart_altitude.Series["Altitude"].Points.AddXY(currentTime, incomingData.Altitude);
+
+                    browser.EvaluateScriptAsync("setmark("+ incomingData.Gps_latitude + ","+ incomingData.Gps_longitude + ");");
 
                     //CreatePointShapefile(axMap1, 0, Convert.ToDouble(strData[3]), Convert.ToDouble(strData[2]));
 
---
+++ Lavira Merkut/Singleton/IncomingDataSingleton.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lavira_Merkut.Singleton
+{
+    class IncomingDataSingleton
+    {
+        private static IncomingDataSingleton instance;
+        private IncomingDataSingleton() { }
+
+        public static IncomingDataSingleton GetInstance()
+        {
+            return instance ?? (instance = new IncomingDataSingleton());
+        }
+
+
+        string teamID;
+        string counter;
+        string altitude;
+        string gps_altitude;
+        string gps_latitude;
+        string gps_longitude;
+        string payload_gps_altitude;
+        string payload_gps_latitude;
+        string payload_gps_longitude;
+        string gyroscope_X;
+        string gyroscope_Y;
+        string gyroscope_Z;
+        string acceleration_X;
+        string acceleration_Y;
+        string acceleration_Z;
+        string angle;
+        string airPressure;
+        string velocity;
+        string state;
+        string crc;
+
+        public string TeamID { get => teamID; set => teamID = value; }
+        public string Counter { get => counter; set => counter = value; }
+        public string Altitude { get => altitude; set => altitude = value; }
+        public string Gps_altitude { get => gps_altitude; set => gps_altitude = value; }
+        public string Gps_latitude { get => gps_latitude; set => gps_latitude = value; }
+        public string Gps_longitude { get => gps_longitude; set => gps_longitude = value; }
+        public string Payload_gps_altitude { get => payload_gps_altitude; set => payload_gps_altitude = value; }
+        public string Payload_gps_latitude { get => payload_gps_latitude; set => payload_gps_latitude = value; }
+        public string Payload_gps_longitude { get => payload_gps_longitude; set => payload_gps_longitude = value; }
+        public string Gyroscope_X { get => gyroscope_X; set => gyroscope_X = value; }
+        public string Gyroscope_Y { get => gyroscope_Y; set => gyroscope_Y = value; }
+        public string Gyroscope_Z { get => gyroscope_Z; set => gyroscope_Z = value; }
+        public string Acceleration_X { get => acceleration_X; set => acceleration_X = value; }
+        public string Acceleration_Y { get => acceleration_Y; set => acceleration_Y = value; }
+        public string Acceleration_Z { get => acceleration_Z; set => acceleration_Z = value; }
+        public string Angle { get => angle; set => angle = value; }
+        public string AirPressure { get => airPressure; set => airPressure = value; }
+        public string Velocity { get => velocity; set => velocity = value; }
+        public string State { get => state; set => state = value; }
+        public string Crc { get => crc; set => crc = value; }
+    }
+}
